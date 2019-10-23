7e2e391aca0d93909e97e91ee2ab0acbef79612b
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-23 23:28:25 +0300

    [ADD] BoxDrawingView

--- app/src/main/java/com/example/draganddraw/DragAndDrawActivity.java
+++ app/src/main/java/com/example/draganddraw/DragAndDrawActivity.java
@@ -1,9 +1,6 @@
 package com.example.draganddraw;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.os.Bundle;
 
 public class DragAndDrawActivity extends SingleFragmentActivity {
 
